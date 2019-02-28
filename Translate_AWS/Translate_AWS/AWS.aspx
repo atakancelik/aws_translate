e62bec414f08546198bc25511994feb6eb5aadcc
Atakan Çelik <atakan@atakancelik.com.tr>
2019-02-28 23:33:53 +0300

    Proje eklendi.

---
+++ Translate_AWS/Translate_AWS/AWS.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AWS.aspx.cs" Inherits="Translate_AWS.AWS" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+   <form id="form1" runat="server">
+        <div class="container">
+            <div class="row">
+                <div class="col ">
+                    <asp:TextBox ID="txt_SourceLanguageCode" Text="tr" BackColor="#999999" CssClass="w-100" runat="server" MaxLength="2"></asp:TextBox>
+                    <asp:Button ID="btn_Swap" class="btn btn-secondary btn-lg btn-block" CssClass="w-100" runat="server" Text="Swap" OnClick="btn_Swap_Click" />
+                    <asp:TextBox ID="txt_TargetLanguageCode" Text="en" BackColor="#999999" CssClass="w-100" runat="server" MaxLength="2"></asp:TextBox>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col">
+                    <asp:TextBox ID="txt_ToTranslate" runat="server" Height="400px"  CssClass="w-100" TextMode="MultiLine"></asp:TextBox>
+                </div>   
+            </div>
+            <div class="row">
+                <div class="col">
+                    <asp:Button  class="btn btn-secondary btn-lg btn-block" ID="btn_Translate" CssClass="w-100" runat="server" Text="Translate" OnClick="btn_Translate_Click" />
+                </div>   
+            </div>
+            <div class="row">
+                <div class="col">
+                    <asp:TextBox ID="txt_Translated" runat="server" Height="400px" CssClass="w-100" ReadOnly="True" TextMode="MultiLine" BackColor="#999999"></asp:TextBox>
+                </div>   
+            </div>
+            <div class="row">
+                <div class="col">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item active">Language Codes</li>
+                <li class="list-group-item">Arabic (ar)</li>
+                <li class="list-group-item">Chinese (zh)</li>
+                <li class="list-group-item">Czech (cs)</li>
+                <li class="list-group-item">French (fr)</li>
+                <li class="list-group-item">German (de)</li>
+                <li class="list-group-item">Italian (it)</li>
+                <li class="list-group-item">Japanese (ja)</li>
+                <li class="list-group-item">Portuguese (pt)</li>
+                <li class="list-group-item">Russian (ru)</li>
+                <li class="list-group-item">Spanish (es)</li>
+                <li class="list-group-item">Turkish (tr)</li>
+                <li class="list-group-item">English (en)</li>
+            </ul>
+        </div>   
+        </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Translate_AWS/Translate_AWS/AWS.aspx.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Amazon;
+using Amazon.Translate;
+using Amazon.Translate.Model;
+
+namespace Translate_AWS
+{
+    public partial class AWS : System.Web.UI.Page
+    {
+        List<string> LanguageCodes = new List<string>
+        {
+            "ar",
+            "zh",
+            "cs",
+            "fr",
+            "de",
+            "it",
+            "ja",
+            "pt",
+            "ru",
+            "es",
+            "tr",
+            "en"
+        };
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        public void Clear()
+        {
+            txt_ToTranslate.Text = "";
+            txt_Translated.Text = "";
+        }
+
+
+        protected void btn_Translate_Click(object sender, EventArgs e)
+        {
+            if (LanguageCodes.Any(x => x == txt_SourceLanguageCode.Text))
+            {
+                if (LanguageCodes.Any(x => x == txt_TargetLanguageCode.Text))
+                {
+                    if (string.IsNullOrEmpty(txt_SourceLanguageCode.Text) ||
+                        string.IsNullOrEmpty(txt_TargetLanguageCode.Text) || string.IsNullOrEmpty(txt_ToTranslate.Text))
+                    {
+                        Clear();
+                        txt_Translated.Text = "! ! ! Fill all fields ! ! !";
+                        txt_ToTranslate.Text = "! ! ! Fill all fields ! ! !";
+                    }
+                    else
+                        txt_Translated.Text = Translate(txt_SourceLanguageCode.Text, txt_TargetLanguageCode.Text,
+                            txt_ToTranslate.Text);
+                }
+                else
+                {
+                    Clear();
+                    txt_Translated.Text = "! ! ! Enter a valid Language Code ! ! !";
+                    txt_ToTranslate.Text = "! ! ! Enter a valid Language Code ! ! !";
+                }
+            }
+            else
+                txt_Translated.Text = "! ! ! Enter a valid Language Code ! ! !";
+                txt_ToTranslate.Text = "! ! ! Enter a valid Language Code ! ! !";
+        }
+
+        protected void btn_Swap_Click(object sender, EventArgs e)
+        {
+           string text = txt_SourceLanguageCode.Text;
+           txt_SourceLanguageCode.Text = txt_TargetLanguageCode.Text;
+           txt_TargetLanguageCode.Text = text;
+        }
+        public string Translate(string SourceLanguageCode, string TargetLanguageCode, string Text)
+        {
+            AmazonTranslateRequest translateRequest = new AmazonTranslateRequest();
+            TranslateTextRequest request = new TranslateTextRequest();
+            IAmazonTranslate amazonTranslate = (IAmazonTranslate)new AmazonTranslateClient("", "", RegionEndpoint.USEast1);
+            request.SourceLanguageCode = SourceLanguageCode;
+            request.TargetLanguageCode = TargetLanguageCode;
+            request.Text = Text;
+            try
+            {
+                return amazonTranslate.TranslateText(request).TranslatedText;
+            }
+            catch (Exception ex)
+            {
+                return "Error " + ex.ToString();
+            }
+        }
+    }
+}
